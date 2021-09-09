f401c1024e7b4d37b949dc0360a49f2ab31ec36d
Zidni Rif'an <zidni.rifan7@gmail.com>
2021-09-09 12:51:56 +0700

    fix LoginUseCase

--- src/Applications/use_case/LoginUseCase.js
+++ src/Applications/use_case/LoginUseCase.js
@@ -1,18 +1,28 @@
 const NewAuth = require('../../Domains/authentications/entities/NewAuth');
 
 class LoginUseCase {
-  constructor({ authenticationRepository, userRepository, tokenManager }) {
+  constructor({
+    authenticationRepository,
+    userRepository,
+    tokenManager,
+    passwordHash,
+  }) {
     this._authenticationRepository = authenticationRepository;
     this._userRepository = userRepository;
     this._tokenManager = tokenManager;
+    this._passwordHash = passwordHash;
   }
 
   async execute(useCasePayload) {
     const newAuth = new NewAuth(useCasePayload);
 
-    const id = await this._userRepository.verifyUserCredential(newAuth);
+    const { id, password: hashedPassword } =
+      await this._userRepository.getUserCredential(newAuth.username);
+    await this._passwordHash.compare(newAuth.password, hashedPassword);
     const accessToken = await this._tokenManager.generateAccessToken({ id });
-    const refreshToken = await this._tokenManager.generateRefreshToken({ id });
+    const refreshToken = await this._tokenManager.generateRefreshToken({
+      id,
+    });
 
     // add refresh token to database
     await this._authenticationRepository.addRefreshToken(refreshToken);
--- src/Applications/use_case/_test/LoginUseCase.test.js
+++ src/Applications/use_case/_test/LoginUseCase.test.js
@@ -3,6 +3,7 @@ const AuthenticationRepository = require('../../../Domains/authentications/Authe
 const UserRepository = require('../../../Domains/users/UserRepository');
 const TokenManager = require('../../security/TokenManager');
 const LoginUseCase = require('../LoginUseCase');
+const PasswordHash = require('../../security/PasswordHash');
 
 describe('LoginUseCase', () => {
   it('should orchestrating the login action correctly', async () => {
@@ -10,6 +11,8 @@ describe('LoginUseCase', () => {
       username: 'jhondoe',
       password: 'password',
     };
+
+    const newAuth = new NewAuth(useCasePayload);
 
     const expectedTokens = {
       accessToken: 'access_token',
@@ -19,39 +22,50 @@ describe('LoginUseCase', () => {
     const mockAuthenticationRepository = new AuthenticationRepository();
     const mockUserRepository = new UserRepository();
     const mockTokenManager = new TokenManager();
+    const mockPasswordHash = new PasswordHash();
 
     mockAuthenticationRepository.addRefreshToken = jest
       .fn()
       .mockImplementation(() => Promise.resolve());
-    mockUserRepository.verifyUserCredential = jest
+    mockUserRepository.getUserCredential = jest
       .fn()
-      .mockImplementation(() => Promise.resolve('user-123'));
+      .mockImplementation(() =>
+        Promise.resolve({ id: 'user-123', password: useCasePayload.password })
+      );
     mockTokenManager.generateAccessToken = jest
       .fn()
       .mockImplementation(() => Promise.resolve(expectedTokens.accessToken));
     mockTokenManager.generateRefreshToken = jest
       .fn()
       .mockImplementation(() => Promise.resolve(expectedTokens.refreshToken));
+    mockPasswordHash.compare = jest
+      .fn()
+      .mockImplementation(() => Promise.resolve());
 
     // creating use case instance
     const loginUseCase = new LoginUseCase({
       authenticationRepository: mockAuthenticationRepository,
       userRepository: mockUserRepository,
       tokenManager: mockTokenManager,
+      passwordHash: mockPasswordHash,
     });
 
     // actions
     const tokens = await loginUseCase.execute(useCasePayload);
 
+    const { password: hashedPassword } =
+      await mockUserRepository.getUserCredential(newAuth.username);
+
     expect(tokens).toStrictEqual(expectedTokens);
     expect(mockAuthenticationRepository.addRefreshToken).toBeCalledWith(
       expectedTokens.refreshToken
     );
-    expect(mockUserRepository.verifyUserCredential).toBeCalledWith(
-      new NewAuth({
-        username: useCasePayload.username,
-        password: useCasePayload.password,
-      })
+    expect(mockUserRepository.getUserCredential).toBeCalledWith(
+      newAuth.username
+    );
+    expect(mockPasswordHash.compare).toBeCalledWith(
+      newAuth.password,
+      hashedPassword
     );
     expect(mockTokenManager.generateAccessToken).toBeCalledWith({
       id: 'user-123',
